c65b9602b9928b49ea16da5755789c9f36ad7981
Tochukwu1159 <christinotochukwu@gmail.com>
2024-07-10 23:05:01 +0100

    task done

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM openjdk:17-jdk-alpine AS builder
+
+# Set working directory for the builder stage
+WORKDIR /app
+
+# Copy the project directory from the context to the working directory
+COPY . .
+
+# Use a multi-stage build to create a slimmer image
+FROM openjdk:17-jre-alpine AS runner
+
+# Set working directory for the runner stage
+WORKDIR /app
+
+# Copy the JAR file from the builder stage
+COPY --from=builder /target/*.jar app.jar
+
+
+# Expose the port where your Spring Boot application listens (replace 8080 with your actual port)
+EXPOSE 8081
+
+# Command to run the Spring Boot application
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/emma/HNG2/util/UserRequest.java
@@ -0,0 +1,32 @@
+package com.emma.HNG2.util;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest
+{
+
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private String phone;
+}
